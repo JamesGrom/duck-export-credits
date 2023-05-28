13b8033df1de6506b539b24a334e320c75f88a9e
James Grom <jgrom@scu.edu>
2023-05-27 22:24:43 -0700

    fix body parsing issue

--- ext/background.js
+++ ext/background.js
@@ -1,32 +1,68 @@
-function getFileEntry(fileSystem, callback) {
-	fileSystem.root.getFile("request_logs.txt", { create: true }, callback, errorHandler);
-}
-
-function appendToFile(fileEntry, content) {
-	fileEntry.createWriter(function (fileWriter) {
-		fileWriter.seek(fileWriter.length); // Move to the end of the file
-		const blob = new Blob([content], { type: "text/plain" });
-		fileWriter.write(blob);
-	}, errorHandler);
-}
-
-function downloadLogs(details) {
+function sendRequestLog(details) {
 	if (details.url.includes("example.com")) {
 		const logEntry = `Request URL: ${details.url}\n`;
-		chrome.fileSystem.chooseEntry({ type: "openWritableFile" }, function (fileEntry) {
-			if (fileEntry) {
-				appendToFile(fileEntry, logEntry);
-			}
-		});
+		console.log(logEntry);
+
+		// Send the log entry to the Node server
+		const serverUrl = "http://localhost:3000/"; // Replace with your server URL
+		fetch(serverUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ fuck: "you" }),
+		})
+			.then((response) => {
+				if (response.ok) {
+					console.log("Log entry sent successfully");
+				} else {
+					console.error("Failed to send log entry:", response.status, response.statusText);
+				}
+			})
+			.catch((error) => {
+				console.error("Failed to send log entry:", error);
+			});
 	}
 }
 
-chrome.webRequest.onBeforeRequest.addListener(downloadLogs, { urls: ["<all_urls>"] }, ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(sendRequestLog, { urls: ["<all_urls>"] }, [
+	"blocking",
+]);
 
 chrome.runtime.onStartup.addListener(function () {
 	chrome.webRequest.handlerBehaviorChanged();
 });
 
-function errorHandler(error) {
-	console.error("Error:", error);
-}
+// // function appendToFile(fileEntry, content) {
+// // 	fileEntry.createWriter(function (fileWriter) {
+// // 		fileWriter.seek(fileWriter.length); // Move to the end of the file
+// // 		const blob = new Blob([content], { type: "text/plain" });
+// // 		fileWriter.write(blob);
+// // 	}, errorHandler);
+// // }
+
+// // function downloadLogs(details) {
+// // 	if (details.url.includes("example.com")) {
+// // 		const logEntry = `Request URL: ${details.url}\n`;
+// // 		chrome.fileSystem.chooseEntry({ type: "openWritableFile" }, function (fileEntry) {
+// // 			if (fileEntry) {
+// // 				appendToFile(fileEntry, logEntry);
+// // 			}
+// // 		});
+// // 	}
+// // }
+
+// function dispatchLogs(details) {
+// 	if (details.url.includes("example.com")) {
+// 		//
+// 	}
+// }
+// chrome.webRequest.onBeforeRequest.addListener(dispatchLogs, { urls: ["<all_urls>"] }, ["blocking"]);
+
+// chrome.runtime.onStartup.addListener(function () {
+// 	chrome.webRequest.handlerBehaviorChanged();
+// });
+
+// function errorHandler(error) {
+// 	console.error("Error:", error);
+// }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,26 @@
-import * as bodyParser from "body-parser";
+// import * as bodyParser from "body-parser";
 import express from "express";
 const app = express();
 const port = 3000;
 app.use((req, res, next): void => {
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 	next();
 });
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-	console.log("bruv");
-	res.json(`home path`);
+// app.get("/", (req, res) => {
+// 	console.log("bruv");
+// 	res.json(`home path`);
+// });
+app.post("/", (req, res) => {
+	console.log("event");
+	// console.log(req);
+	console.log(req.body);
+	res.json(`succeeded`);
 });
 app.listen(port, () => {
 	console.log(`express server started listening on ${port}`);
