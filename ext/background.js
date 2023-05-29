5cd7a0721f2e3645f5799af2a1f5d50f4e56b130
James Grom <jgrom@scu.edu>
2023-05-28 18:15:52 -0700

    semi-stable status of logging to mainPanel console

--- ext/background.js
+++ ext/background.js
@@ -1,13 +1,1 @@
 console.log("Duck-Export-Credits extension backgroundjs loaded");
-// Listen to network response events
-// chrome.webRequest.onCompleted.addListener(
-// 	function (details) {
-// 		console.log("webRequestBody details from backgroundjs triggered");
-// 		// Get the response body of the completed network request
-// 		chrome.tabs.sendMessage(details.tabId, {
-// 			action: "logResponseBody",
-// 			requestId: details.requestId,
-// 		});
-// 	},
-// 	{ urls: ["<all_urls>"], types: ["main_frame"] }
-// );
