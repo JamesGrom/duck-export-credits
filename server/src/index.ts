7e121d42b4cfa5859345636a25a63847c9f24629
James Grom <jgrom@scu.edu>
2023-05-29 13:18:30 -0700

    cursory type interface for zoominfo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import * as fs from "fs";
+import { createObjectCsvWriter } from "csv-writer";
 const app = express();
 const port = 3005;
 app.use((req, res, next): void => {
@@ -11,9 +13,16 @@ app.use((req, res, next): void => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// https://www.linkedin.com/sales-api/salesApiProfiles/(profileId:ACwAAA0DrUwBVYgKNPX9HNcbd6mXP2VOX1aji6c,authType:NAME_SEARCH,authToken:UbPM)?decoration=%28entityUrn%2CobjectUrn%2CfirstName%2ClastName%2CfullName%2Cheadline%2CmemberBadges%2ClatestTouchPointActivity%2Cpronoun%2Cdegree%2CprofileUnlockInfo%2Clocation%2ClistCount%2Csummary%2CsavedLead%2CdefaultPosition%2CcontactInfo%2CcrmStatus%2CpendingInvitation%2Cunlocked%2CflagshipProfileUrl%2CfullNamePronunciationAudio%2Cmemorialized%2CnumOfConnections%2CnumOfSharedConnections%2CshowTotalConnectionsPage%2Cpositions*%28companyName%2Ccurrent%2Cnew%2Cdescription%2CendedOn%2CposId%2CstartedOn%2Ctitle%2Clocation%2CrichMedia*%2CcompanyUrn~fs_salesCompany%28entityUrn%2Cname%2CcompanyPictureDisplayImage%29%29%2CcrmManualMatched%29
+
 app.post("/", (req, res) => {
 	console.log("event");
 	console.log("event body: ", req.body);
+	const existingData = fs.readFileSync("../output.json", "utf-8");
+	const jsonData = JSON.parse(existingData);
+	jsonData.push(req.body.data);
+	const updatedData = JSON.stringify(jsonData);
+	fs.writeFileSync("../output.json", updatedData, "utf-8");
 	res.json(`succeeded`);
 });
 app.listen(port, () => {
---
+++ server/src/zoominfo/parseviewContacts.ts
@@ -0,0 +1,78 @@
+type CompanyAddress = {
+	Street?: string;
+	City?: string;
+	State?: string;
+	Zip?: string;
+	CountryCode?: string;
+};
+
+type DoziIndustry = {
+	name?: string;
+	displayName?: string;
+	isPrimary?: boolean;
+	score?: number;
+};
+
+type Certification = {
+	certificationName?: string;
+	organizationName?: string;
+};
+
+type LocationOfPerson = {
+	City?: string;
+	State?: string;
+	CountryCode?: string;
+};
+
+type CompanyType = "PRIVATE" | "PUBLIC";
+
+type Person = {
+	companyID?: number;
+	companyName?: string;
+	companyAddress?: CompanyAddress;
+	companyRevenue?: string;
+	companyRevenueRange?: string;
+	companyEmployees?: number;
+	companyEmployeeCountRange?: string;
+	companyDomain?: string;
+	website?: string;
+	companyDescription?: string;
+	companyRevenueIn000s?: number;
+	doziIndustry?: DoziIndustry[];
+	companyType?: CompanyType;
+	certified?: boolean;
+	topLevelIndustry?: string[];
+	isMasked?: boolean;
+	subscribed?: boolean;
+	title?: string;
+	jobTitle?: string;
+	isPast?: string;
+	personID?: number;
+	firstName?: string;
+	lastName?: string;
+	email?: string;
+	location?: LocationOfPerson;
+	lastUpdatedDate?: string;
+	lastMentioned?: string;
+	confidenceScore?: number;
+	name?: string;
+	score?: number;
+	certifications?: Certification[];
+	isEU?: boolean;
+	creationDate?: string;
+	positionStartDate?: string;
+	buyingCommittee?: boolean;
+	hasOnlinePresence?: boolean;
+	personHashtags?: string[];
+	profileImageURL?: string;
+	freeMails?: string[];
+	noticeProvidedInfo?: {
+		hasBeenNotified?: boolean;
+		emailNoticeProvidedDate?: string;
+	};
+	personalEmail?: string;
+};
+
+const parseviewContacts = () => {
+	//
+};
