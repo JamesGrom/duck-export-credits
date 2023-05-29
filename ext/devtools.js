9c6cb62938f1e1793a882948400b09fab30249c3
James Grom <jgrom@scu.edu>
2023-05-28 17:52:11 -0700

    proper panel creation & callback for chrome extension main panel

--- ext/devtools.js
+++ ext/devtools.js
@@ -2,13 +2,11 @@
 console.log("Duck-Export-Credits extension devtools.js loaded");
 
 // Mount the main panel
-chrome.devtools.panels
-	.create(
-		"Duck Export Credits", // title
-		"", // icon
-		"/panels/mainPanel/MainPanel.html" // content
-	)
-	.then((newPanel) => {
-		newPanel.onShown.addListener(initialisePanel);
-		newPanel.onHidden.addListener(unInitialisePanel);
-	});
+chrome.devtools.panels.create(
+	"Duck Export Credits", // title
+	"", // icon
+	"/panels/mainPanel/MainPanel.html", // content,
+	function (panel) {
+		console.log(`create panel callback triggered, with response ${panel}`);
+	}
+);
